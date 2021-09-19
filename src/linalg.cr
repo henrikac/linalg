a39582ed8180d0c94b750a85a91b7f7d0620725f
Henrik Christensen <sensen1695@hotmail.com>
2021-09-20 00:03:31 +0200

    updated comments

--- src/linalg.cr
+++ src/linalg.cr
@@ -1,8 +1,6 @@
 require "./linalg/**"
 
-# TODO: Write documentation for `Linalg`
+# `Linalg` module contains `Linalg::Vector` and `Linalg::Matrix`.
 module Linalg
   VERSION = "0.1.0"
-
-  # TODO: Put your code here
 end
